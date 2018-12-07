39db47276c4b71bcfa8d484945db2beb262a7f87
JoeVincentt <eugene.bb@hotmail.com>
2018-12-06 21:54:57 -0500

    remove control added

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,13 +34,27 @@ class BurgerBuilder extends Component {
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
   };
 
-  removeIngridientHandler = type => {};
+  removeIngridientHandler = type => {
+    const oldCount = this.state.ingredients[type];
+    const updatedCount = oldCount - 1;
+    const updatedIngredients = {
+      ...this.state.ingredients
+    };
+    updatedIngredients[type] = updatedCount;
+    const priceDeduction = INGREDIENT_PRICES[type];
+    const oldPrice = this.state.totalPrice;
+    const newPrice = oldPrice - priceDeduction;
+    this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+  };
 
   render() {
     return (
       <Aux>
         <Burger ingredients={this.state.ingredients} />
-        <BuildControls ingredientAdded={this.addIngridientHandler} />
+        <BuildControls
+          ingredientAdded={this.addIngridientHandler}
+          ingredientRemoved={this.removeIngridientHandler}
+        />
       </Aux>
     );
   }
