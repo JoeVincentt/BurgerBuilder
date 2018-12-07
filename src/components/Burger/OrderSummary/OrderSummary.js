4529c59d49f7677f9fcbf3bc2d5dbd98dc1a9a2f
JoeVincentt <eugene.bb@hotmail.com>
2018-12-07 13:49:47 -0500

    button components added for easy styling

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -18,6 +18,8 @@ const orderSummary = props => {
       <p>A delicious burger with the folowing ingredients:</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to CheckOut</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </Aux>
   );
 };
