8186bfa9d0c7e0de82a6e1a6c5813b154ecebecd
JoeVincentt <eugene.bb@hotmail.com>
2018-12-08 00:24:32 -0500

    SideDrawer backdrop added and goes away on click

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,18 +2,23 @@ import React from "react";
 import classes from "./SideDrawer.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigarionItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
 
 const sideDrawer = props => {
   return (
-    <div className={classes.SideDrawer}>
-      <div>
-        <Logo />
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={classes.SideDrawer}>
+        <div>
+          <Logo />
+        </div>
+        <hr />
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <hr />
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
