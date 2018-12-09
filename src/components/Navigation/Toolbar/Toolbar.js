a7a0d374c60043d21375647d77afd7ffdde8e791
JoeVincentt <eugene.bb@hotmail.com>
2018-12-08 23:33:35 -0500

    added DraweToggle element, little styling changed

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigarionItems/NavigationItems";
+import DrawerToggle from "../SideDrawer/DrawerToggle/DrawerToggle";
 
 const toolbar = props => (
   <header className={classes.Toolbar}>
-    <div>MENU</div>
+    <DrawerToggle open={props.open} />
     <div className={classes.LogoMobileOnly}>
       <Logo />
     </div>
